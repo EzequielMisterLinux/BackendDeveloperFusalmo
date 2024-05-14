f4884deaad9ef479232e65440658fb88235339e1
EzequielMisterLinux <humbertoezequiel.z.c@gmail.com>
2024-05-14 15:03:14 -0600

    agregando nuevos ejercicios a la clase4 ejercicios New promise finalizado

---
+++ Modulo1/clase4/ejercicio5.js
@@ -0,0 +1,10 @@
+function operacionAsync(callback) {
+    setTimeout(() => {
+        callback('operacion async completada')
+        
+    }, 2000);
+}
+
+operacionAsync((mensaje) => {
+    console.log(mensaje);
+})
